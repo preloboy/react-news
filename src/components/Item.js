8dddcb1de6f98344ab5d6932cfa92a5d79060cc7
Prelo Tulshi <72853954+preloboy@users.noreply.github.com>
2023-04-12 17:04:08 +0530

    Updated the Next and previous button

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 
 export class Item extends Component {
-    render() {
-        const { title, description, image, url } = this.state;
-        return (
-            <div className="card" style={{width: '18rem'}}>
-                <img src={image} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href={url} className="btn btn-sm btn-primary">Go somewhere</a>
-                    </div>
-            </div>
-        )
-    }
+  render() {
+    const { content } = this.props;
+    return (
+      // console.log(content.title)
+      <div className="card" style={{width: '18rem'}}>
+          <img src={content.urlToImage} className="card-img-top" alt="..."/>
+              <div className="card-body">
+                  <h5 className="card-title">{content.title}</h5>
+                  <p className="card-text">{content.description}</p>
+                  <a href={content.url} className="btn btn-sm btn-primary">Go somewhere</a>
+              </div>
+      </div>
+    )
+  }
 }
 
 export default Item
--- src/components/News.js
+++ src/components/News.js
@@ -5,7 +5,8 @@ class News extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      articles: []
+      articles: [],
+      page: 1
     };
   }
 
@@ -14,26 +15,54 @@ class News extends Component {
   }
 
   async fetchArticles() {
-    const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c3becb3f7a534a20956deb6f9446f892');
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c3becb3f7a534a20956deb6f9446f892&page=${this.state.page}`);
     const jsonData = await response.json();
     this.setState({ articles: jsonData.articles });
+  }
+
+  clickPrev = async () => {
+    // console.log("clicked on Previous")
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c3becb3f7a534a20956deb6f9446f892&page=${this.state.page - 1}`);
+    const jsonData = await response.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: jsonData.articles
+    })
+
+  }
+
+  clickNext = async () => {
+    // console.log("clicked on Next")
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c3becb3f7a534a20956deb6f9446f892&page=${this.state.page + 1}`);
+    const jsonData = await response.json();
+    console.log(jsonData)
+    this.setState({
+      page: this.state.page + 1,
+      articles: jsonData.articles
+
+    })
   }
 
   render() {
     const { articles } = this.state;
     return (
-      <div>
-        <h1>Top Business Headlines:</h1>
-        <ul>
+      <div className='container'>
+        <h3 className='mt-3'>Top Business Headlines:</h3>
+        <div className="row">
           {articles.map((article, index) => (
-            <div key={index} className="col-md-4">
-              <Item title={article.title ? article.title : ""} description={article.description ? article.description : ""} image={article.urlToImage} url={article.url} />
+            <div className="col-md-4 my-3" key={index}>
+              <Item content={article} />
             </div>
           ))}
-        </ul>
+        </div>
+        <div className="d-flex justify-content-between">
+          <button disabled={this.state.page <= 1} onClick={this.clickPrev} type="button" className="btn btn-primary">&larr; Previous</button>
+          <button onClick={this.clickNext} type="button" className="btn btn-primary">Next &rarr;</button>
+        </div>
       </div>
     );
   }
 }
+
 
 export default News
